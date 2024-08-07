86137141e0ff15776337d9a86cde31d7750f54e9
Pjvl99 <pablovelasquez@ufm.edu>
2024-08-06 22:55:26 -0600

    Updating request module

--- request.py
+++ request.py
@@ -1,8 +1,18 @@
 import requests
 import json
+from zipfile import ZipFile 
+import os
 
-
+current_working_directory = os.getcwd()
 response = requests.get("https://ocds.guatecompras.gt/files")
 data = response.json()
 for value in data['result']:
-    print(value)
+    response = requests.get(value['files']['csv'])
+    break
+
+with open("files.zip", mode="wb") as file:
+    file.write(response.content)
+
+with ZipFile("files.zip", "r") as zips:
+    zips.extractall(current_working_directory)
+
